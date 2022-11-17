0fd5a5a4faf1727650d7d6bd2ccb0123bb5af6fa
Thiago Souza <thiagorasouza@gmail.com>
2022-11-17 13:48:54 -0300

    refactor: move sut creation to a factory helper method

--- src/modules/users/useCases/createUser.spec.ts
+++ src/modules/users/useCases/createUser.spec.ts
@@ -2,17 +2,34 @@ import { Result } from "../core/result";
 import { CreateUserUseCase } from "./createUser";
 import { UserRepository } from "./userRepository";
 
-class UserRepositoryMock implements UserRepository {
-  async getUserByEmail(email: string): Promise<any> {
-    return Promise.resolve(Promise.resolve(Result.fail("User does not exist")));
+const makeUserRepositoryMock = (): UserRepository => {
+  class UserRepositoryMock implements UserRepository {
+    async getUserByEmail(email: string): Promise<any> {
+      return Promise.resolve(
+        Promise.resolve(Result.fail("User does not exist"))
+      );
+    }
   }
+
+  return new UserRepositoryMock();
+};
+
+interface SutTypes {
+  sut: CreateUserUseCase;
+  userRepositoryMock: UserRepository;
 }
 
-describe("CreateUser Test Suite", () => {
+const makeSut = (): SutTypes => {
+  const userRepositoryMock = makeUserRepositoryMock();
+  const sut = new CreateUserUseCase(userRepositoryMock);
+
+  return { sut, userRepositoryMock };
+};
+
+describe("CreateUserUseCase Test Suite", () => {
   it("should check if email is already registered", () => {
-    const userRepository = new UserRepositoryMock();
-    const sut = new CreateUserUseCase(userRepository);
-    const getUserByEmailSpy = jest.spyOn(userRepository, "getUserByEmail");
+    const { sut, userRepositoryMock } = makeSut();
+    const getUserByEmailSpy = jest.spyOn(userRepositoryMock, "getUserByEmail");
     const request = {
       email: "valid_email",
       password: "valid_password",
