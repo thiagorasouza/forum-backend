bdd01e4d58e9d250eb4c19dd6a41895fc319783e
Thiago Souza <thiagorasouza@gmail.com>
2022-11-20 12:12:14 -0300

    feat: add ExpressHttpView implementation

---
+++ src/modules/users/main/ExpressHttpView.test.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from "express";
+import request from "supertest";
+import { CreateUserHttpViewModel } from "../useCases/createUser/createUserHttpViewModel";
+import { ExpressHttpView } from "./ExpressHttpView";
+
+const app = express();
+
+const mockViewModel: CreateUserHttpViewModel = {
+  statusCode: 200,
+  body: "test",
+};
+
+describe("ExpressHttpView Test Suite", () => {
+  it("should return the correct HTTP status code", async () => {
+    app.get("/test_status_code", (req: Request, res: Response): void => {
+      const sut = new ExpressHttpView(res);
+      sut.display(mockViewModel);
+    });
+
+    await request(app).get("/test_status_code").expect(200);
+  });
+
+  it("should return JSON", async () => {
+    app.get("/test_json", (req: Request, res: Response): void => {
+      const sut = new ExpressHttpView(res);
+      sut.display(mockViewModel);
+    });
+
+    await request(app).get("/test_json").expect("content-type", /json/);
+  });
+
+  it("should return the correct body", async () => {
+    app.get("/test_body", (req: Request, res: Response): void => {
+      const sut = new ExpressHttpView(res);
+      sut.display(mockViewModel);
+    });
+
+    await request(app)
+      .get("/test_body")
+      .expect(JSON.stringify(mockViewModel.body));
+  });
+});
