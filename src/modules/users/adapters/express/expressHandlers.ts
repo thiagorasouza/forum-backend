d1d2be794423e2c7be6c307ef967232c359008be
Thiago Souza <thiagorasouza@gmail.com>
2022-11-20 13:12:36 -0300

    feat: add express handler for create user usecase

---
+++ src/modules/users/adapters/express/expressHandlers.ts
@@ -0,0 +1,19 @@
+import { CreateUserHttpController } from "../../useCases/createUser/createUserHttpController";
+import { CreateUserHttpPresenter } from "../../useCases/createUser/createUserHttpPresenter";
+import { CreateUserUseCase } from "../../useCases/createUser/createUserUseCase";
+import { SequelizeUserRepository } from "../../adapters/sequelize/sequelizeUserRepository";
+import { Response, Request } from "express";
+import { ExpressView } from "./expressView";
+
+export const createUserExpressHandler = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const view = new ExpressView(res);
+  const presenter = new CreateUserHttpPresenter(view);
+  const repository = new SequelizeUserRepository();
+  const useCase = new CreateUserUseCase(repository, presenter);
+  const controller = new CreateUserHttpController(useCase);
+  await controller.handle(req.body);
+  return res.end();
+};
