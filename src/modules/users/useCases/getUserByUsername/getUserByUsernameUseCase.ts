a13c2102999cb42780eedcec2451d8906d8ddd73
Thiago Souza <thiagorasouza@gmail.com>
2022-11-22 10:17:32 -0300

    feat: ensure that GetUserByUsernameUseCase fails with ServerError if repository throws

--- src/modules/users/useCases/getUserByUsername/getUserByUsernameUseCase.ts
+++ src/modules/users/useCases/getUserByUsername/getUserByUsernameUseCase.ts
@@ -1,13 +1,4 @@
-// import { Success } from "../../core/success";
-// import { User } from "../../domain/user";
-// import { UserData } from "../../domain/userData";
-// import { EmailAlreadyRegisteredFailure } from "./createUserFailures";
-// import { ServerFailure } from "../failures/serverFailure";
-// import { CreateUserPresenter } from "./createUserPresenter";
-// import { CreateUserRepository } from "./createUserRepository";
-// import { CreateUserRequestModel } from "./createUserRequestModel";
-// import { CreateUserResponseModel } from "./createUserResponseModel";
-
+import { ServerFailure } from "../failures/serverFailure";
 import { GetUserByUsernamePresenter } from "./getUserByUsernamePresenter";
 import { GetUserByUsernameRepository } from "./getUserByUsernameRepository";
 import { GetUserByUsernameRequestModel } from "./getUserByUsernameRequestModel";
@@ -21,18 +12,14 @@ export class GetUserByUsernameUseCase {
 
   async execute(request: GetUserByUsernameRequestModel): Promise<void> {
     const { username } = request;
-    // try {
-    const getUserByUsernameResult = await this.repository.getByUsername(
-      username
-    );
-    return this.toPresenter(getUserByUsernameResult);
-
-    // const userModel = getUserByUsernameResult.value;
-    //   await this.repository.create(userModel);
-    //   return this.toPresenter(new Success<string>("User created"));
-    // } catch (error) {
-    //   return this.toPresenter(new ServerFailure());
-    // }
+    try {
+      const getUserByUsernameResult = await this.repository.getByUsername(
+        username
+      );
+      return this.toPresenter(getUserByUsernameResult);
+    } catch (error) {
+      return this.toPresenter(new ServerFailure());
+    }
   }
 
   private toPresenter(response: GetUserByUsernameResponseModel) {
